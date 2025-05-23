affac86ed5e60b2ee598a54de30626de2ae37f9d
HNS2243 <131307906+HNS2243@users.noreply.github.com>
2025-05-23 02:28:53 -0300

    Add files via upload

---
+++ VagonDeCarga.php
@@ -0,0 +1,57 @@
+<?php
+class VagonDeCarga extends Vagon {
+    private float $pesoMaximoTransporte;
+    private float $pesoCarga;
+    private float $indice;
+
+    public function __construct(DateTime $anioInstalacion, float $largo, float $ancho, float $pesoVacio, float $pesoMaximoTransporte) {
+        parent::__construct($anioInstalacion, $largo, $ancho, $pesoVacio);
+        $this->pesoMaximoTransporte = $pesoMaximoTransporte;
+        $this->indice = 0;
+    }
+
+    // Getters
+    public function getPesoMaximoTransporte(): float {
+        return $this->pesoMaximoTransporte;
+    }
+
+    public function getPesoCarga(): float {
+        return $this->pesoCarga;
+    }
+    public function getIndice(): float {
+        return $this->indice;
+    }
+
+    // Setters
+    public function setPesoMaximoTransporte(float $pesoMaximoTransporte): void {
+        $this->pesoMaximoTransporte = $pesoMaximoTransporte;
+    }
+    public function setIndice($pesoCarga){
+        $indice = $pesoCarga * .20;
+        $this->indice = $indice;
+    }
+
+    public function setPesoCarga(float $pesoCarga): bool {
+        $pudio = false;
+        $this->setIndice($pesoCarga);
+        $indice = $this->getIndice();
+        $pesoCargaFinal = $pesoCarga + $indice;
+        $pesoCargaMax = $this->getPesoMaximoTransporte();
+        if ($pesoCargaFinal <= $pesoCargaMax){
+        $this->pesoCarga = $pesoCargaFinal;
+        $this->calcularPesoVagon($pesoCargaFinal); // actualiza el peso total
+        $pudio = true;
+        }
+        return $pudio;
+    }
+
+    // Redefinición del método __toString
+    public function __toString(): string {
+        return parent::__toString() .
+           "Peso máximo de carga: " . $this->pesoMaximoTransporte . "kg\n" .
+           "Peso de la carga actual (sin índice): " . ($this->pesoCarga - $this->indice) . "kg\n" .
+           "Índice de carga (20%): " . $this->indice . "kg\n" .
+           "Peso total de carga (con índice): " . $this->pesoCarga . "kg\n";
+    }
+}
+?>
---
+++ VagonDePasajeros.php
@@ -0,0 +1,56 @@
+<?php
+class VagonDePasajeros extends Vagon {
+    private int $maxPasajeros;
+    private int $pasajerosTransportados;
+    private float $pesoPromedioPasajero;
+
+    public function __construct(DateTime $anioInstalacion, float $largo, float $ancho, float $pesoVacio, int $maxPasajeros){
+        parent::__construct($anioInstalacion, $largo, $ancho, $pesoVacio);
+        $this->maxPasajeros = $maxPasajeros;
+        $this->pesoPromedioPasajero = 0;
+        $this->pasajerosTransportados = 0;
+    }
+
+    // Getters
+    public function getMaxPasajeros(): int {
+        return $this->maxPasajeros;
+    }
+
+    public function getPasajerosTransportados(): int {
+        return $this->pasajerosTransportados;
+    }
+
+    public function getPesoPromedioPasajero(): float {
+        return $this->pesoPromedioPasajero;
+    }
+
+    // Setters
+    public function setMaxPasajeros(int $maxPasajeros): void {
+        $this->maxPasajeros = $maxPasajeros;
+    }
+
+    public function setPesoPromedioPasajero(float $pesoPromedioPasajero): void {
+        $this->pesoPromedioPasajero = $pesoPromedioPasajero;
+    }
+
+    // Método para cargar pasajeros
+    public function incorporarPasajeroVagon($pasajerosIngresantes): bool {
+        $puedeCargar = false;
+        $pasajerosMaxTemp = $this->getMaxPasajeros();
+        if ($pasajerosIngresantes <= $pasajerosMaxTemp) {
+            $this->pasajerosTransportados = $pasajerosIngresantes;
+            $pesoTotalPasajeros = $pasajerosIngresantes * $this->pesoPromedioPasajero;
+            $this->setPesoVagon($this->getPesoVacio() + $pesoTotalPasajeros);
+            $puedeCargar = true;
+        }
+        return $puedeCargar;
+    }
+
+    // Método toString
+    public function __toString(): string {
+        return parent::__toString() .
+               "Tipo: Vagón de pasajeros\n" .
+               "Pasajeros actuales: " . $this->pasajerosTransportados . " / " . $this->maxPasajeros . "\n" .
+               "Peso promedio por pasajero: " . $this->pesoPromedioPasajero . "kg\n";
+    }
+}
